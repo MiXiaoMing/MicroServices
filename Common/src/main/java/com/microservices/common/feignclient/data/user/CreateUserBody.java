0029feaaa9837a291655f4a5f21eec97b1c27f21
mixiaoming <mixiaoming@qiumiapp.com>
2020-06-03 20:40:07 +0800

    删除冗余文件

--- Common/src/main/java/com/microservices/common/feignclient/data/user/CreateUserBody.java
+++
@@ -1,8 +0,0 @@
-package com.microservices.common.feignclient.data.user;
-
-import java.io.Serializable;
-import java.util.Date;
-
-public class CreateUserBody implements Serializable {
-    public String name, type, phoneNumber;
-}
