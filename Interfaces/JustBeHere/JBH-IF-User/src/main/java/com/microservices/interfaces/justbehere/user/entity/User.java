ec8deba492cb2f2fa74d9422656bb495d9584b51
mixiaoming <mixiaoming@qiumiapp.com>
2020-06-12 19:00:40 +0800

    删除无效代码

--- Interfaces/JustBeHere/JBH-IF-User/src/main/java/com/microservices/interfaces/justbehere/user/entity/User.java
+++
@@ -1,9 +0,0 @@
-package com.microservices.interfaces.justbehere.user.entity;
-
-import java.io.Serializable;
-import java.util.Date;
-
-public class User implements Serializable {
-	public String id, loginID, telephone;
-	public Date updateTime, createTime;
-}
