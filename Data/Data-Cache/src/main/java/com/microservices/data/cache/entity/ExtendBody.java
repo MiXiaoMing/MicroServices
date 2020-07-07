4b85eb6c2c301ace7d26165d2fa9b36aa5b4bcf3
mixiaoming <mixiaoming@qiumiapp.com>
2020-07-07 14:38:40 +0800

    移动文件位置

--- Data/Data-Cache/src/main/java/com/microservices/data/cache/entity/ExtendBody.java
+++
@@ -1,8 +0,0 @@
-package com.microservices.data.cache.entity;
-
-import java.io.Serializable;
-
-public class ExtendBody implements Serializable {
-    public String key, value;
-    public int seconds;
-}
--- Data/Data-Cache/src/main/java/com/microservices/data/cache/entity/ExtendResult.java
+++
@@ -1,8 +0,0 @@
-package com.microservices.data.cache.entity;
-
-import java.io.Serializable;
-
-public class ExtendResult implements Serializable {
-    public String key, value;
-    public long ttl;
-}
