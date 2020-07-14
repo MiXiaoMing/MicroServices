462c164f75731e2a5548ee73e73b03295af88801
mixiaoming <mixiaoming@qiumiapp.com>
2020-07-14 09:46:58 +0800

    调整注册中心插件：eureka -> nacos

--- Gateway/Zuul/src/main/java/com/microservices/gateway/zuul/ZuulApplication.java
+++ Gateway/Zuul/src/main/java/com/microservices/gateway/zuul/ZuulApplication.java
@@ -3,13 +3,13 @@ package com.microservices.gateway.zuul;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableZuulProxy
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableCircuitBreaker
 @EnableFeignClients(basePackages = "com.microservices")
 public class ZuulApplication {
--- Interfaces/JustBeHere-IF/src/main/java/com/microservices/interfaces/justbehere/JbhIfApplication.java
+++ Interfaces/JustBeHere-IF/src/main/java/com/microservices/interfaces/justbehere/JbhIfApplication.java
@@ -3,11 +3,11 @@ package com.microservices.interfaces.justbehere;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableCircuitBreaker
 @EnableFeignClients(basePackages = "com.microservices")
 public class JbhIfApplication {
